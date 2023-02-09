11d7e6a8a9e964e70e5fde60395f4125499bd95c
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-02-09 19:30:48 +0100

    fix img style

--- resources/views/components/product.blade.php
+++ resources/views/components/product.blade.php
@@ -1,6 +1,6 @@
 <div class="col-12 col-md-4 col-lg-2 d-flex flex-column myCard">
     <div class="img">
-        <img src={{$product -> img}} alt="">
+        <img class="img-fluid" src={{$product -> img}} alt="" >
     </div>
     <h4>{{$product -> name}}</h4>
     <p>{{$product -> description}}</p>
