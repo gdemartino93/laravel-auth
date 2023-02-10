9ee1747bcf0498585b332b84b46a2e4570259a08
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-02-10 18:04:38 +0100

    test 2

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -6,7 +6,7 @@
     <div class="container">
         <div class="col-12 d-flex flex-column justify-content-center text-center">
             <h1>Ecco la lista di tutti i prodotti</h1>
-            <p>Ricordati di registrarti per accedere ai prodotti in sconto.</p>
+            <p class="fw-bold">Ricordati di registrarti per accedere ai prodotti in sconto.</p>
             <div class="wrapper">
                 @foreach ($products as $product)
                     @include('components.product')
